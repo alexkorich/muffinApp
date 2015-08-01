90ba296563e22a45cf7d843437b7c167f5441c9d
Alexey Korichkovskiy <johniezigger@gmail.com>
2015-08-01 23:53:22 +0300

    role on create user added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
+  validates  :email, :password,  presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-	ROLES = %w[admin customer]
-	has_many :orders
+  :recoverable, :rememberable, :trackable, :validatable
+  ROLES = %w[admin customer]
+  has_many :orders
+
+  before_save :first_admin
+
+  private
+  def first_admin
+    if User.exists?
+      self.role="customer"
+    else
+      self.role="admin"
+    end  
+  end
 end
