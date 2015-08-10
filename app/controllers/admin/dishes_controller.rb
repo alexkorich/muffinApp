f3b3f1f85fc21140e5ece306f26b349a7300c435
Alexey Korichkovskiy <johniezigger@gmail.com>
2015-08-11 01:01:53 +0300

    creating dishes

--- app/controllers/admin/dishes_controller.rb
+++ app/controllers/admin/dishes_controller.rb
@@ -1,9 +1,30 @@
 class Admin::DishesController < ApplicationController
 layout 'admin/layouts/application'
  def index
+  @dishes=Dish.all
   end
 
   def show
+  end
+
+  def create
+    puts "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs"
+    puts params
+    @dish=Dish.new
+    @dish.name=params[:dish][:name]
+    @dish.type=params[:dish][:type]
+    @dish.quantity=params[:dish][:quantity]
+    @dish.price=params[:dish][:price]
+
+    respond_to do |format|
+      if @dish.save
+        format.html { redirect_to :back, notice: 'Dish was successfully created.' }
+        format.json { render :show, status: :created, location: @dish }
+      else
+        format.html {  redirect_to :back, notice: 'Something went wrong!' }
+        format.json { render json: @dish.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
