a93d5a9cb0fa5f3e483432948ec1bc200b7ee8c4
Alexey Korichkovskiy <johniezigger@gmail.com>
2015-12-03 18:41:05 +0200

    mailer

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = { 
-  :address              => 'smtp.gmail.com',
-  :port                 => 587,
-  :domain               => 'gmail.com',
-  :user_name            => 'EMAIL_ADDRESS@gmail.com',
-  :password             => 'pass',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-  }
